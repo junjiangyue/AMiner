5e583d737cfd2451f98bdba01e802c07f4a52714
LWF0824 <985579124@qq.com>
2022-06-11 23:48:18 +0800

    关键作者

--- AMiner/src/views/else/searchField.vue
+++ AMiner/src/views/else/searchField.vue
@@ -3,14 +3,14 @@
 		<div class="bread-crumb">
 			<el-breadcrumb separator=">>">
 				<el-breadcrumb-item><span class="el-icon-menu"></span> 其它</el-breadcrumb-item>
-				<el-breadcrumb-item>邻域查询</el-breadcrumb-item>
+				<el-breadcrumb-item>领域查询</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
 		<div class="page-body">
       <div class="dataSearch">
         <el-card class="box-card">
           <el-row>
-            <el-col :span="3">邻域查询</el-col>
+            <el-col :span="3">领域查询</el-col>
             <el-col :span="8">
               请选择查询对象：<el-select v-model="value" placeholder="请选择查询对象">
                 <el-option
@@ -23,25 +23,50 @@
             </el-col>
             <el-col :span="8">
               <el-col :span="9">
-                请输入查询邻域：
+                请输入查询领域：
               </el-col>
               <el-col :span="15">
-                <el-input v-model="input" placeholder="请输入查询邻域"></el-input>
+                <el-input v-model="input" placeholder="请输入查询领域"></el-input>
               </el-col>
             </el-col>
             <el-col :span="5">
-              <el-button class="mar-left">查询</el-button>
+              <el-button class="mar-left" @click="getSubject()">查询</el-button>
             </el-col>
           </el-row>
         </el-card>
         <el-row>
-          <el-col :span="5">
-            <el-card class="information">
-            </el-card>
+          <el-col :span="6">
+            <el-table :data="tableData" stripe style="width: 100%">
+              <el-table-column prop="researchInterest" label="领域" width="180">
+              </el-table-column>
+              <el-table-column fixed="right" label="操作" width="100">
+                <template slot-scope="scope">
+                  <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
           </el-col>
-          <el-col :span="19">
-            <el-card class="graph">
-            </el-card>
+          <el-col :span="18">
+            <el-row><p class="txt-field">查询领域：{{field}}</p></el-row>
+            <el-row v-if="value=='Author'">
+              <el-table :data="authorTable" stripe style="width: 100%">
+                <el-table-column prop="authorName" label="作者姓名">
+                </el-table-column>
+                <el-table-column prop="pc" label="发表文章总数">
+                </el-table-column>
+                <el-table-column prop="cn" label="文章被引用总数">
+                </el-table-column>
+                <el-table-column prop="hi" label="H-index">
+                </el-table-column>
+                <el-table-column prop="pi" label="等A-index的P-index">
+                </el-table-column>
+                <el-table-column prop="upi" label="不等A-index的P-index">
+                </el-table-column>
+                <el-table-column prop="authorId" label="作者ID">
+                </el-table-column>
+              </el-table>
+            </el-row>
+            
           </el-col>
         </el-row>
       </div>
@@ -53,162 +78,61 @@
 		data () {
 			return {
 				input: '',
-        authorName: 'Bob Jolls',
             // 防止出现多个echarts初始化的情况
         myChart: '',
         options: [{
-          value: '选项1',
+          value: 'Author',
           label: '关键作者'
         }, {
-          value: '选项2',
+          value: 'Affliation',
           label: '关键单位'
         }, {
-          value: '选项3',
-          label: '关键期刊'
-        }, {
-          value: '选项4',
-          label: '关键会议'
+          value: 'PublicationVenue',
+          label: '关键期刊或会议'
         }],
-        value: ''
+        value: '',
+        tableData: [],
+        authorTable: [],
+        field:"",
 			}
 		},
-        mounted:function(){
-            this.getData();
-        },
-        methods: {
-            getData() {
-                this.$axios({
+    methods: {
+      getSubject() {
+        this.$axios({
           method:"get",
-          url: 'http://localhost:3000/getCooperationByName',
+          url: 'http://localhost:3000/findResearchInterest',
           params:{
-            authorName:this.authorName
+            researchInterest:this.input,
+            entity:this.value
           }
         }).then(res=>{
           console.log(res);
+          this.tableData=res.data;
+          console.log(res.data);
         },err=>{
           console.log(err);
         })
-            },
-
-            // 绘制知识图谱
-        getGraph(p1, p2) {
-            // 若已经存在有初始化了的echarts实例，就直接进行绘制
-            if(this.myChart) {
-                this.configGraph(p1, p2);
-            }else {
-                // 没有初始化的echarts实例，就初始化一个
-                this.myChart = this.$echarts.init(this.$refs.graph);
-                this.configGraph(p1, p2);
-            }
-            
-        },
-
-            // 绘制知识图谱的配置项
-            configGraph(p1, p2) {
-            // 保存传进来的节点和关系数据
-            let mydata = p1;
-            let links = p2;
-            
-            // 图谱的配置项
-            let option = {
-                    // 提示框的配置
-                    tooltip: {
-                        trigger: 'item'//设置提示框的内容和格式 节点和边都显示name属性
-                    },
-                    //图形上的文本标签，可用于说明图形的一些数据信息
-                    label: {
-                        fontSize: 12
-                    },
-                    legend: {
-                        x: "center",
-                        show: true
-                    },
-                    series: [
-                        {
-                            type: 'graph',// 类型:关系图
-                            layout: 'force',//图的布局，类型为力导图
-                            symbolSize: 50,//节点大小
-                            emphasis: {
-                                focus: 'adjacency'
-                            },//当鼠标移动到节点上，突出显示节点以及节点的边和邻接节点
-                            draggable: true,//指示节点是否可以拖动
-                            roam: true,
-                            // 两端的样式（无 / 箭头）
-                            edgeSymbol: ['none', 'arrow'],
-                            // 不同节点的颜色之类的配置
-                            categories: [{
-                                name: '电影',
-                                itemStyle: {
-                                        color: "lightgreen"
-                                }
-                            }, {
-                                name: '主演',
-                                itemStyle: {
-                                        color: "orange",
-                                }
-                            }, {
-                                name: '类型',
-                                itemStyle: {
-                                        color: "pink",
-                                }
-                            }, {
-                                name: '导演',
-                                itemStyle: {
-                                        color: "lightblue",
-                                }
-                            },{
-                                name: 'TMDbID',
-                                itemStyle: {
-                                        color: "#fcce4c",
-                                }
-                            }],
-                            // 节点上的文字
-                            label: {
-                                    show: true,
-                                    fontSize: 12,
-                                    color: "black",
-                            },
-                            force: {
-                                repulsion: 1200,//节点之间的斥力因子。支持数组表达斥力范围，值越大斥力越大。
-                                gravity: 0.1, //节点受到的向中心的引力因子。该值越大节点越往中心点靠拢。
-                            },
-                            edgeSymbolSize: [4, 6], // 边两端的标记(箭头)大小，可以是一个数组分别指定两端，也可以是单个统一指定。
-                            // 边上显示的文字
-                            edgeLabel: {
-                                    show: true,
-                                    fontSize: 12,
-                                    formatter: "{c}"
-                            },
-                            // 节点数据
-                            data: mydata,
-                            // 关系数据
-                            links: links,
-                            // 边的样式
-                            lineStyle: {
-                                    opacity: 0.9,
-                                    width: 1.1,
-                                    curveness: 0,
-                                    color: "#262626",
-                            }
-                        }
-                    ]
-            };
-
-            //节点自定义拖拽不回弹
-            const chart = this.myChart;
-            chart.on('mouseup', function (params) {
-                var option = chart.getOption();
-                option.series[0].data[params.dataIndex].x = params.event.offsetX;
-                option.series[0].data[params.dataIndex].y = params.event.offsetY;
-                option.series[0].data[params.dataIndex].fixed = true;
-                chart.setOption(option);
-            });
-
-            // 使用刚指定的配置项和数据显示图表。
-            this.myChart.setOption(option);
-        }
-	}
-}
+      },
+      handleClick(index) {
+        console.log('index:',index);
+        console.log(index.researchInterest);
+        this.field=index.researchInterest;
+        this.$axios({
+          method:"get",
+          url: 'http://localhost:3000/findKeyAuthor',
+          params:{
+            researchInterest:index.researchInterest
+          }
+        }).then(res=>{
+          console.log("关键作者：",res);
+          this.authorTable=res.data;
+          console.log(res.data);
+        },err=>{
+          console.log(err);
+        })
+      },
+    }
+  }
 </script>
 <style scoped>
 .dataSearch {
@@ -228,15 +152,12 @@
     width: 100%;
     height: 100px;
   }
-  .information {
-      width: 100%;
-    height: 500px;
-  }
-  .graph {
-      width: 100%;
-    height: 500px;
-  }
   .mar-left {
     margin-left: 30px;
+  }
+  .txt-field {
+    text-align:center;
+    margin-top: 20px;
+    margin-bottom: 20px;
   }
 </style>
